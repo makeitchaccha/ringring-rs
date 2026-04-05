a9d48f341f872b36149e7be5483c0502ad45b592
yuyaprgrm <yuyaprgrm@outlook.jp>
2026-04-05 21:40:39 +0900

    feat: improve performance splitting activity (#12)
    * feat: improve performance splitting activity into activity and screen-sharing
    
    * fix cs

--- src/reporting/types.rs
+++ src/reporting/types.rs
@@ -1,4 +1,4 @@
-use crate::room::{Activity, Moment, ParticipantLease, RoomLease, UserIdentity};
+use crate::room::{AudioActivity, Interval, Moment, ParticipantLease, RoomLease, UserIdentity};
 use serenity::all::{
     ChannelId, CreateAttachment, CreateComponent, CreateMessage, EditAttachments, EditMessage,
     GuildId, Http, MessageFlags, MessageId,
@@ -31,23 +31,33 @@ impl RoomSnapshot {
 }
 
 #[derive(Debug, Clone)]
+pub struct HistorySnapshot {
+    pub audio: Vec<AudioActivity>,
+    pub screen_sharing: Vec<Interval>,
+}
+
+#[derive(Debug, Clone)]
 pub struct ParticipantSnapshot {
     pub identity: UserIdentity,
-    pub history: Vec<Activity>,
+    pub history: HistorySnapshot,
 }
 
 impl ParticipantSnapshot {
     fn from_lease(value: ParticipantLease) -> Self {
         Self {
             identity: value.identity,
-            history: value.history.to_vec(),
+            history: HistorySnapshot {
+                audio: value.history.audio.to_vec(),
+                screen_sharing: value.history.screen_sharing.to_vec(),
+            },
         }
     }
 
     pub fn calculate_duration(&self, now: Instant) -> Duration {
         self.history
+            .audio
             .iter()
-            .map(|activity| activity.calculate_duration(now))
+            .map(|activity| activity.interval.calculate_duration(now))
             .sum()
     }
 }
