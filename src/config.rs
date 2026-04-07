01b3b85d97d0acc8d30ece9d2e2fbe57c18033cb
yuyaprgrm <yuyaprgrm@outlook.jp>
2026-04-07 09:21:49 +0900

    Feat: introduce configuration (#17)
    * feat: introduce configuration
    
    * fix: use Token instead of just String

---
+++ src/config.rs
@@ -0,0 +1,37 @@
+use anyhow::Context;
+use config::{Config, Environment, File};
+use serde::Deserialize;
+use serenity::all::ChannelId;
+use serenity::prelude::Token;
+use std::path::Path;
+
+pub fn load_config(path: &Path) -> anyhow::Result<AppConfig> {
+    let config = Config::builder()
+        .add_source(File::from(path))
+        .add_source(Environment::with_prefix("RINGRING"))
+        .build()
+        .context("failed to build config")?;
+
+    let app_config: AppConfig = config
+        .try_deserialize()
+        .context("failed to deserialize config")?;
+
+    Ok(app_config)
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct AppConfig {
+    pub discord: DiscordConfig,
+    pub subscriptions: Vec<SubscriptionEntry>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct DiscordConfig {
+    pub token: Token,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct SubscriptionEntry {
+    pub voice_channel: ChannelId,
+    pub report_channel: ChannelId,
+}
